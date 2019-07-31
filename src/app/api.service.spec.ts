b5b43839700eb9e2800e96c4ef494df08c8075d5
BasilMawejje <basil.mawejje@gmail.com>
2019-07-31 18:18:38 +0300

    Add api service test for 404 error Add api service test to verify simulation of http request

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 // Setup for tests
 interface Product {
@@ -38,18 +38,34 @@ fdescribe('ApiService', () => {
 
   it('can get products via HttpClient GET method', () => {
     const testProduct: Product = {id: 1, name: 'Bibimbap', price: 49, description: 'The best we offer.'};
-
     httpClient.get<Product>(testUrl)
       .subscribe(data =>
         expect(data).toEqual(testProduct)
       );
 
     const req = httpTestingController.expectOne('/data');
-
     expect(req.request.method).toEqual('GET');
-
     req.flush(testProduct);
-
     httpTestingController.verify();
+  });
+
+  it('httpTestingController.verify should fail if HTTP response not simulated', () => {
+    httpClient.get('some/api').subscribe();
+    expect(() => httpTestingController.verify()).toThrow();
+    const req = httpTestingController.expectOne('some/api');
+    req.flush(null);
+  });
+
+  it('can test for 404 error', () => {
+    const emsg = 'deliberate 404 error';
+    httpClient.get<Product[]>(testUrl).subscribe(
+      data => fail('should have failed with the 404 error'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404, 'status');
+        expect(error.error).toEqual(emsg, 'message');
+      }
+    );
+    const req = httpTestingController.expectOne(testUrl);
+    req.flush(emsg, { status: 404, statusText: 'Not Found' });
   });
 });
