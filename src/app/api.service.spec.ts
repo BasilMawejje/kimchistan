322deaaa2cced1bba01c9b6258d43374db919902
BasilMawejje <basil.mawejje@gmail.com>
2019-07-31 18:01:32 +0300

    Add api service tests for creating the service Add api service tests for http get Both are green

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,12 +1,55 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
+import { HttpClient } from '@angular/common/http';
 
-describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+// Setup for tests
+interface Product {
+  id: number, 
+  name: string, 
+  price: number,
+  description: string
+}
 
+const testUrl = '/data';
+
+fdescribe('ApiService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ], 
+      providers: [ ApiService ]
+    });
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  // Tests begin here
   it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
-    expect(service).toBeTruthy();
+    const apiService: ApiService = TestBed.get(ApiService);
+    expect(apiService).toBeTruthy();
+  });
+
+  it('can get products via HttpClient GET method', () => {
+    const testProduct: Product = {id: 1, name: 'Bibimbap', price: 49, description: 'The best we offer.'};
+
+    httpClient.get<Product>(testUrl)
+      .subscribe(data =>
+        expect(data).toEqual(testProduct)
+      );
+
+    const req = httpTestingController.expectOne('/data');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(testProduct);
+
+    httpTestingController.verify();
   });
 });
