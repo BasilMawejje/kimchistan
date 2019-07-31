8efd054f709c467c3ab2a9d766d6ad18db466742
BasilMawejje <basil.mawejje@gmail.com>
2019-07-31 10:31:53 +0300

    Add detail view to root component Add mock products Able to select and display a single product on product component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from "../product";
+import { PRODUCTS } from '../mock-products';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
+
 export class ProductsComponent implements OnInit {
 
-  product: Product = {
-    id: 1,
-    name: 'Bibimbap'
-  };
+  products = PRODUCTS;
+  selectedProduct: Product;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(product: Product): void {
+    this.selectedProduct = product;
+  }
 }
