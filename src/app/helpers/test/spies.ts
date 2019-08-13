dc3a777a590e04dfeec41b0bdee90f8845d9b00c
BasilMawejje <basil.mawejje@gmail.com>
2019-08-13 10:31:15 +0300

    Remove unused helpers directory

--- src/app/helpers/test/spies.ts
+++
@@ -1,8 +0,0 @@
-const createSpyObj = (name: string, methods: string[]) => {
-    return jasmine.createSpyObj(name, methods);
-  };
-
-export const ApiServiceSpy = createSpyObj('ApiService', [
-    'getProducts',
-    'getProduct',
-  ]);
