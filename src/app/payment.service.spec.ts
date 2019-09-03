f78360c991c991a96fc3250a8e7f5b5e44020d57
BasilMawejje <basil.mawejje@gmail.com>
2019-09-03 12:47:35 +0300

    Fix failing tests

--- src/app/payment.service.spec.ts
+++ src/app/payment.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PaymentService } from './payment.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PaymentService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ]
+  }));
 
   it('should be created', () => {
     const service: PaymentService = TestBed.get(PaymentService);
